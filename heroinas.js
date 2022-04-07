483501c3ebf00a171987722bb6b8f0fe1f29cd90
apwill <anaplsssw@gmail.com>
2022-04-06 23:29:59 -0300

    second commit

---
+++ heroinas.js
@@ -0,0 +1,15 @@
+//usando console.log
+
+function heroinasMarvel (heroina1, heroina2, heroina3) {
+    console.log(`Suas heroínas escolhidas foram ${heroina1}, ${heroina2}, ${heroina3}`)
+}
+
+heroinasMarvel("Wanda", "Viúva-Negra", "Capitã Marvel")
+
+//usando return
+
+function heroinasMarvel2 (heroina1, heroina2, heroina3) {
+    return `Suas heroínas escolhidas foram ${heroina1}, ${heroina2}, ${heroina3}`
+}
+
+console.log(heroinasMarvel2("Wanda", "Viúva-Negra", "Capitã Marvel"))
